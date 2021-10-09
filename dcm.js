cc31ad7547b50219064b204d59eaa1b9291a8dce
Sheila Strahan <smstrahan@gmail.com>
2021-10-09 16:09:20 -0600

    fixed piano key menu and added sound

---
+++ dcm.js
@@ -0,0 +1,15 @@
+
+    var cNote = new Audio()
+    cNote.src="/sound/C.mp3";
+
+    var dNote = new Audio()
+    dNote.src="/sound/D.mp3";
+
+    var eNote = new Audio()
+    eNote.src="/sound/E.mp3";
+
+    var cSharpNote = new Audio()
+    cSharpNote.src="/sound/Csharp.mp3";
+
+    var dSharpNote = new Audio()
+    dSharpNote.src="/sound/dsharp.mp3";
