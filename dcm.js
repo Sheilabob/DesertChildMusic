7ef2a7ad96d2a8b5477ff3e30463069f75db7d16
Sheila Strahan <smstrahan@gmail.com>
2021-10-18 19:14:20 -0600

    live links in footer, added spotify fa, payment modal, start of resources links to youtube, game insert of hooray and try again cards

--- dcm.js
+++ dcm.js
@@ -20,6 +20,7 @@
     let fCard = document.getElementById("fCard")
     let gCard = document.getElementById("gCard")
     let aCard = document.getElementById("aCard")
+    let hooray = document.getElementById("hooray")
 
     let cardArray = [cCard, dCard, eCard, fCard, gCard, aCard];
     let deletedCard;
@@ -30,14 +31,92 @@
             $("#signUpModal").modal("show");
         });
 
+        $("#paymentButton").click(function(){
+            $("#paymentModal").modal("show");
+        });
+
+
         $("#btnShowCard").click(function(){
            $(deletedCard).collapse("hide");
+           $(hooray).collapse("hide");
+           $(tryAgain).collapse("hide");
             let randomInteger = Math.floor(Math.random() * (cardArray.length-1));
-            console.log(randomInteger);
             $(cardArray[randomInteger]).collapse("show");
             deletedCard = cardArray[randomInteger];
             cardArray.splice(randomInteger, 1);
             console.log(cardArray);
             console.log(deletedCard);
         })
+
+        $("#cNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === cCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#dNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === dCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#eNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === eCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#fNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === fCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#gNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === gCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#aNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === aCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+        $("#bNote").click(function(){
+            $(hooray).collapse("hide");
+            $(tryAgain).collapse("hide");
+            if (deletedCard === bCard) {
+                $(hooray).collapse("show");
+            } else {
+                $(tryAgain).collapse("show");
+            }
+        })
+
+
     });
